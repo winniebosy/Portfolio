a8ca5f404fd7f5afc32984e80d35e5358509c3b6
winniebosy <winniebosy@gmail.com>
2020-01-23 14:09:31 +0300

    deleted the exisitng js files for a new one

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
 
-//Triggers the page load event
-document.addEventListener('DOMContentLoaded', () =>{
-  //Get all navbar-burger elements
-  const $navbarBurgers = Array.prototype.slice.call (document.querySelectorAll( '.navbar-burger'), 0);
-
-//Checks if there is any navbar burgers
-
-if ($navbarBurgers.length > 0){
-    //Adds a click event on each of the navbar-burger elements
-
-    $navbarBurgers.forEach( el => { 
-        
-        el.addEventListener('click', () => {
-        //Gets the largest from the data-target attribute
-
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        //toogles the is-active class on both navbar burger and navbar menu
-        el.classList.toggle('is-active');
-        $target.classList('is-active')
 
 
 
 
-    });
-        
-    });
-}
 
-
-
-
-});
 
