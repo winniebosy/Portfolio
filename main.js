9c1c16848dad022019e11630deda4e4bc7a1ebec
winniebosy <winniebosy@gmail.com>
2020-01-23 14:21:59 +0300

    variable declarations

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+'use strict'
 
+const nav = document.querySelector('#navbar');
+const menu =document.querySelector('#menu');
+const menuToggle = document.querySelector('.navbar-burger');
+let isMenuOpen = false;
 
 
 
