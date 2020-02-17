a7cdc5db71b47fbf700fee938bdfbf2e7bbd85f7
winniebosy <winniebosy@gmail.com>
2020-02-17 11:27:50 +0300

    deleted the navbar section of js file

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-'use strict'
-
-const nav = document.querySelector('#nav');
-const menu =document.querySelector('#menu');
-const menuToggle = document.querySelector('.nav-toggle');
-let isMenuOpen = false;
 
 
-//toggle menu active state
-
-menuToggle.addEventListener('click', e => {
-  e.preventDefault();
-  isMenuOpen = !isMenuOpen;
-
-  //toggle a11y attributes and active class
-
-  menuToggle.setAttribute('aria-expanded' , String(isMenuOpen));
-  menu.hidden =  !isMenuOpen;
-  nav.classList.toggle('nav--open');
-
-});
 
 
 
